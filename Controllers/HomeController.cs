a0969d5792505c60c8fe7c78752acb1419286a29
Maurya <Prabhat.Maurya@allscripts.com>
2019-05-04 23:15:35 +0530

    Added the ci/cd top menu

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,10 @@ namespace TADAAzureDemo.Controllers
         {
             return View();
         }
+        public IActionResult AutoCICD()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
